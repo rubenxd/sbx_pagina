c09d2dc9be80fe128dca02e63763b2e7d9ea92ac
ruben dario cordoba cuellar <ruben0793@hotmail.com>
2022-09-20 11:55:52 -0500

    inicio pagina web

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './component/inicio/inicio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'inicio', component:InicioComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './component/nav-bar/nav-bar.component';
+import { InicioComponent } from './component/inicio/inicio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule,
